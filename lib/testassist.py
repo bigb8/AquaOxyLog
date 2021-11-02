ee4ac6b2184abf9dc92ee86d5736a3c6ffbb42d3
Morten Bo Svendsen <mortenbosvendsen@gmail.com>
2021-11-02 06:36:11 +0100

    Update testassist.py

--- lib/testassist.py
+++ lib/testassist.py
@@ -1,17 +1,17 @@
 import assist as ass
 
 #Setsettings
-settings = {
-    "saveloc_data": "",
-    "saveloc_latest": "",
-    "deviceport": "COM3",
-    "channelsactive": 2
-}
+# settings = {
+#     "saveloc_data": "",
+#     "saveloc_latest": "",
+#     "deviceport": "COM3",
+#     "channelsactive": 2
+# }
+#
+
+# ass.savesettings(settings)
 
 
-ass.savesettings(settings)
-
-
-saveloc_data, saveloc_latest, deviceport, channelsactive = ass.getsettings()
+saveloc_data, saveloc_latest, deviceport, channelsactive = ass.getsettings2()
 
 print(saveloc_data, saveloc_latest, deviceport, channelsactive)
